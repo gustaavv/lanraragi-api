0947143f725727f3f2262dcf141baa65f85f6371
gustav <gustaavv.git@yahoo.com>
2026-04-21 12:46:22 +0800

    build: migrate to uv

--- src/lanraragi_api/base/__init__.py
+++ src/lanraragi_api/base/__init__.py
@@ -1,4 +1,4 @@
-from lanraragi_api.base.archive import Archive, ArchiveAPI, ArchiveMetadata
+from lanraragi_api.base.archive import ArchiveAPI, ArchiveMetadata
 from lanraragi_api.base.base import (
     APIError,
     APIHttpError,
@@ -9,11 +9,10 @@ from lanraragi_api.base.base import (
     MinionJobResponse,
     OperationResponse,
 )
-from lanraragi_api.base.category import Category, CategoryAPI, CategoryMetadata
+from lanraragi_api.base.category import CategoryAPI, CategoryMetadata
 from lanraragi_api.base.database import (
     BackupArchiveMetadata,
     BackupCategoryMetadata,
-    BackupTankoubonMetadata,
     DatabaseAPI,
     DatabaseBackup,
 )
@@ -22,7 +21,6 @@ from lanraragi_api.base.misc import MiscAPI
 from lanraragi_api.base.search import SearchAPI
 from lanraragi_api.base.shinobu import ShinobuAPI
 from lanraragi_api.base.tankoubon import (
-    Tankoubon,
     TankoubonAPI,
     TankoubonDetailResponse,
     TankoubonListResponse,
