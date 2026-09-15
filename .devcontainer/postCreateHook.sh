a90e79cbde185c14232fcd52572f01db8695c02d
gustav <gustaavv.git@yahoo.com>
2026-09-15 05:17:15 +0000

    chore: find code exe in post create hook in devcontainer

--- .devcontainer/postCreateHook.sh
+++ .devcontainer/postCreateHook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# code is not available at this stage because it is still in the container creation phase
+# a workaround, see:
+# https://github.com/microsoft/vscode-remote-release/issues/8535#issuecomment-1825413099
+alias code="$(ls ~/.vscode-server*/bin/*/bin/code-server* | head -n 1)"
+
 # remove useless extensions bundled with ms-python.python
 code --install-extension ms-python.python
 code --uninstall-extension ms-python.debugpy --uninstall-extension ms-python.vscode-python-envs
