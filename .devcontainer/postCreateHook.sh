c000100c534451747cf956adf7200c845f4d26c2
gustav <gustaavv.git@yahoo.com>
2026-09-15 05:26:35 +0000

    chore: update post create hook for devcontainer

--- .devcontainer/postCreateHook.sh
+++ .devcontainer/postCreateHook.sh
@@ -3,10 +3,11 @@
 # code is not available at this stage because it is still in the container creation phase
 # a workaround, see:
 # https://github.com/microsoft/vscode-remote-release/issues/8535#issuecomment-1825413099
-alias code="$(ls ~/.vscode-server*/bin/*/bin/code-server* | head -n 1)"
+# note that the path to code-server has changed
+code="$(ls ~/.vscode-remote/bin/*/bin/code-server* | head -n 1)"
 
 # remove useless extensions bundled with ms-python.python
-code --install-extension ms-python.python
-code --uninstall-extension ms-python.debugpy --uninstall-extension ms-python.vscode-python-envs
+$code --install-extension ms-python.python
+$code --uninstall-extension ms-python.debugpy --uninstall-extension ms-python.vscode-python-envs
 
 make install
