385c4f60ef5b430719ed7dae4603c518d16f4ae6
gustav <gustaavv.git@yahoo.com>
2026-09-15 05:09:10 +0000

    chore: use post create hook to install python extension in devcontainer

--- .devcontainer/postCreateHook.sh
+++ .devcontainer/postCreateHook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # remove useless extensions bundled with ms-python.python
+code --install-extension ms-python.python
 code --uninstall-extension ms-python.debugpy --uninstall-extension ms-python.vscode-python-envs
 
 make install
