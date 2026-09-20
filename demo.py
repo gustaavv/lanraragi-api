5825fd4230bda844902d34c69ce03e5f16e47b92
gustav <gustaavv.git@yahoo.com>
2026-09-20 06:44:25 +0000

    docs: update readme and remove demo.py

--- demo.py
+++
@@ -1,9 +0,0 @@
-from lanraragi_api import LANraragiAPI
-from lanraragi_api.base.archive import ArchiveMetadata
-
-apikey = "your-key"
-server = "http://127.0.0.1:3000"
-api = LANraragiAPI(server, key=apikey)
-
-archives: list[ArchiveMetadata] = api.search.get_random_archives()
-print(archives[0])
